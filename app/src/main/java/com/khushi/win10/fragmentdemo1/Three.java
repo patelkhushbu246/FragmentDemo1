585957b35300564289328af47e11013ad94a74c8
khushbupatel246 <khushbupatel2497@yahoo.com>
2017-02-06 13:48:17 +0530

    frag commit

---
+++ app/src/main/java/com/khushi/win10/fragmentdemo1/Three.java
@@ -0,0 +1,21 @@
+package com.khushi.win10.fragmentdemo1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Win10 on 01-02-2017.
+ */
+
+public class Three extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.three,container,false);
+        return view;
+    }
+}
